e4cf4c3dd59f20a9c2efd1c41ef3019cfaa577d0
James16-ops <baluyutjamespatrick347@gmail.com>
2026-07-28 23:57:46 +0800

    Implement typing effect for display text
    Added a typing effect for the text 'Aspiring Programmer'.

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("Portfolio website loaded successfully!");
+const text = "Aspiring Programmer";
+const typingElement = document.getElementById("typing");
+
+let index = 0;
+
+function typeText() {
+
+    if(index < text.length){
+
+        typingElement.innerHTML += text.charAt(index);
+
+        index++;
+
+        setTimeout(typeText, 100);
+
+    }
+
+}
+
+typeText();
