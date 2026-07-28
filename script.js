5d97b2aeea553ebcbc1bda78ca4e93d52f61c2d2
James16-ops <baluyutjamespatrick347@gmail.com>
2026-07-29 00:26:45 +0800

    Update script.js

--- script.js
+++ script.js
@@ -4,17 +4,21 @@ const typingElement = document.getElementById("typing");
 let index = 0;
 
 function typeText() {
-
     if(index < text.length){
-
         typingElement.innerHTML += text.charAt(index);
-
         index++;
-
         setTimeout(typeText, 100);
-
     }
-
 }
 
 typeText();
+
+document.addEventListener("mousemove", (e) => {
+
+    const x = (window.innerWidth / 2 - e.clientX) / 50;
+    const y = (window.innerHeight / 2 - e.clientY) / 50;
+
+    document.body.style.backgroundPosition =
+        `${50 + x}% ${50 + y}%`;
+
+});
